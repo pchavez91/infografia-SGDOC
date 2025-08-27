1a59f67e262c21b5f6ce2e459c777e3f81654643
PatricioChavez <pchavez.dev@gmail.com>
2025-08-27 12:41:56 -0400

    en cascada se dejo busqueda

--- forms/cascada.php
+++ forms/cascada.php
@@ -126,31 +126,51 @@
       .panel-header {
         background-color: #39b3d7;
         border: 1px solid #269abc;
-        padding: 10px;
+        padding: 10px 15px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
         position: relative;
+        height: 60px;
+        overflow: hidden;
       }
 
-      .panel-header b {
+      .panel-header .titulo-panel {
         color: white;
-        font-size: 16px;
+        font-size: 34px;
+        font-weight: bold;
+        text-align: center;
+        flex-grow: 1;
+      }
+
+      #btnBusquedaRapida {
+        margin-right: 10px;
       }
 
       .icono-ayuda {
-        width: 48px;
-        height: 48px;
+        width: 70px;
+        height: 64px;
         cursor: pointer;
         position: absolute;
-        top: -4px;
-        right: -2px;
+        top: -3px;
+        right: 0px;
       }
+
+      .dataTables_length,
+      .dataTables_filter {
+        display: none !important;
+      }
+
 
     </style>
   </head>
   <body>
     <div class="panel-header">
-      <b><center>EXPLORADOR DE ARCHIVOS</center></b>
+      <button id="btnBusquedaRapida" class="btn btn-primary">Buscar archivo</button>
+      <div class="titulo-panel">EXPLORADOR DE ARCHIVOS</div>
       <img id="btnAyuda" onclick="abre_modal_ayuda()" src="IMAGENES/ayuda.png" alt="Ayuda" class="icono-ayuda" title="¿Necesitas ayuda?">
     </div>
+
     <br>
 
     <button id="btnVolver"
@@ -263,4 +283,81 @@
       });
     });
   </script>
+
+  <!-- busqueda rapida -->
+  <div id="ventana_busqueda_archivo" class="modal fade">
+    <div class="modal-dialog modal-lg">
+      <div class="modal-content">
+                  
+        <div class="modal-header" style="display: flex; align-items: center; justify-content: space-between;">
+          
+          <img src="../../images/photos/logo_comasa_avatar.png" alt="Logo Comasa" style="height: 40px; margin-right: 10px;">
+          
+          <div style="flex-grow: 1; text-align: center;">
+            <h2 class="modal-title" style="margin: 0;">BUSCADOR</h2>
+          </div>
+          
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close" style="margin-left: 10px;">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+
+        <form id="form_editar_descripcion" role="form">
+          <br>
+          <div class="modal-body">
+            <div class="form-group mb-3">
+              <h4 class="font-weight-bold mb-2" style="color:#333;">🔎 Buscador de archivos</h4>
+              <input type="text" id="buscador" class="form-control" placeholder="Escribe para encontrar tus archivos..." onkeyup="tablaFiltroGlobal(this.value)">
+            </div>
+            <h4 class="font-weight-bold mb-2" style="color:#333;">🧮 Filtros</h4>
+
+            <div class="form-row mb-3">
+              <div class="col-md-4">
+                <select id="selectRepositorio" class="form-control">
+                  <option value="">Todos los repositorios</option>
+                </select>
+              </div>
+              <div class="col-md-4">
+                <select id="selectArea" class="form-control">
+                  <option value="">Seleccione repositorio primero</option>
+                </select>
+              </div>
+              <div class="col-md-4">
+                <select id="selectDepartamento" class="form-control">
+                  <option value="">Seleccione área primero</option>
+                </select>
+              </div>
+            </div>
+
+          <div class="row" style="padding: 0 15px;">
+            <div class="col-xs-12 col-lg-12">
+              <table
+                id="tabla_lista_archivos_encontrados"
+                class="display nowrap"
+                width="100%">
+                <thead>
+                  <tr>
+                    <th>NOMBRE</th>
+                    <th>CODIGO</th>
+                    <th>VER</th>
+                    <th>RUTA</th>
+                  </tr>
+                </thead>
+              </table>
+            </div>
+          </div>
+          <br><br>
+        </form>
+
+        <div class="modal-footer">
+          <button
+            type="button"
+            class="btn btn-default"
+            data-dismiss="modal">Salir</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
 </html>
